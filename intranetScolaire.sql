d31dec72adfc45fee0eed2a988a99f09460aa680
marcptkwds <marc.ptk@gmail.com>
2017-05-18 11:43:00 +0200

    change .sql

--- intranetScolaire.sql
+++ intranetScolaire.sql
@@ -8,19 +8,19 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
 -- -----------------------------------------------------
--- Schema mydb
+-- Schema mvc_intranet
 -- -----------------------------------------------------
 
 -- -----------------------------------------------------
--- Schema mydb
+-- Schema mvc_intranet
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
-USE `mydb` ;
+CREATE SCHEMA IF NOT EXISTS `mvc_intranet` DEFAULT CHARACTER SET utf8 ;
+USE `mvc_intranet` ;
 
 -- -----------------------------------------------------
--- Table `mydb`.`Users`
+-- Table `mvc_intranet`.`Users`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Users` (
+CREATE TABLE IF NOT EXISTS `mvc_intranet`.`Users` (
   `id_user` INT NOT NULL AUTO_INCREMENT,
   `user_email` VARCHAR(150) NOT NULL,
   `user_firstname` VARCHAR(45) NULL,
@@ -37,9 +37,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Absences`
+-- Table `mvc_intranet`.`Absences`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Absences` (
+CREATE TABLE IF NOT EXISTS `mvc_intranet`.`Absences` (
   `id_absence` INT NOT NULL AUTO_INCREMENT,
   `id_user` INT NOT NULL,
   `absence_started_at` DATETIME NULL,
@@ -51,16 +51,16 @@ CREATE TABLE IF NOT EXISTS `mydb`.`Absences` (
   INDEX `fk_Absences_Users1_idx` (`id_user` ASC),
   CONSTRAINT `fk_Absences_Users1`
     FOREIGN KEY (`id_user`)
-    REFERENCES `mydb`.`Users` (`id_user`)
+    REFERENCES `mvc_intranet`.`Users` (`id_user`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Promotions`
+-- Table `mvc_intranet`.`Promotions`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Promotions` (
+CREATE TABLE IF NOT EXISTS `mvc_intranet`.`Promotions` (
   `id_promotion` INT NOT NULL AUTO_INCREMENT,
   `promotion_name` VARCHAR(15) NOT NULL,
   PRIMARY KEY (`id_promotion`))
@@ -68,9 +68,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Specialities`
+-- Table `mvc_intranet`.`Specialities`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Specialities` (
+CREATE TABLE IF NOT EXISTS `mvc_intranet`.`Specialities` (
   `id_speciality` INT NOT NULL AUTO_INCREMENT,
   `speciality_name` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`id_speciality`))
@@ -78,9 +78,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Students`
+-- Table `mvc_intranet`.`Students`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Students` (
+CREATE TABLE IF NOT EXISTS `mvc_intranet`.`Students` (
   `id_student` INT NOT NULL AUTO_INCREMENT,
   `id_user` INT NOT NULL,
   `id_promotion` INT NOT NULL,
@@ -91,26 +91,26 @@ CREATE TABLE IF NOT EXISTS `mydb`.`Students` (
   INDEX `fk_Students_Specialities1_idx` (`id_speciality` ASC),
   CONSTRAINT `fk_Students_Users1`
     FOREIGN KEY (`id_user`)
-    REFERENCES `mydb`.`Users` (`id_user`)
+    REFERENCES `mvc_intranet`.`Users` (`id_user`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Students_Promotions1`
     FOREIGN KEY (`id_promotion`)
-    REFERENCES `mydb`.`Promotions` (`id_promotion`)
+    REFERENCES `mvc_intranet`.`Promotions` (`id_promotion`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Students_Specialities1`
     FOREIGN KEY (`id_speciality`)
-    REFERENCES `mydb`.`Specialities` (`id_speciality`)
+    REFERENCES `mvc_intranet`.`Specialities` (`id_speciality`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Subjects`
+-- Table `mvc_intranet`.`Subjects`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Subjects` (
+CREATE TABLE IF NOT EXISTS `mvc_intranet`.`Subjects` (
   `id_subject` INT NOT NULL AUTO_INCREMENT,
   `subject_name` VARCHAR(150) NOT NULL,
   `subject_description` TEXT NULL,
@@ -119,9 +119,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Staffs`
+-- Table `mvc_intranet`.`Staffs`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Staffs` (
+CREATE TABLE IF NOT EXISTS `mvc_intranet`.`Staffs` (
   `id_staff` INT NOT NULL AUTO_INCREMENT,
   `id_user` INT NOT NULL,
   `staff_name` VARCHAR(45) NOT NULL,
@@ -129,16 +129,16 @@ CREATE TABLE IF NOT EXISTS `mydb`.`Staffs` (
   INDEX `fk_Staffs_Users1_idx` (`id_user` ASC),
   CONSTRAINT `fk_Staffs_Users1`
     FOREIGN KEY (`id_user`)
-    REFERENCES `mydb`.`Users` (`id_user`)
+    REFERENCES `mvc_intranet`.`Users` (`id_user`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Teachers`
+-- Table `mvc_intranet`.`Teachers`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Teachers` (
+CREATE TABLE IF NOT EXISTS `mvc_intranet`.`Teachers` (
   `id_teacher` INT NOT NULL AUTO_INCREMENT,
   `id_user` INT NOT NULL,
   `id_speciality` INT NOT NULL,
@@ -147,21 +147,21 @@ CREATE TABLE IF NOT EXISTS `mydb`.`Teachers` (
   INDEX `fk_Teachers_Specialities1_idx` (`id_speciality` ASC),
   CONSTRAINT `fk_Teachers_Users1`
     FOREIGN KEY (`id_user`)
-    REFERENCES `mydb`.`Users` (`id_user`)
+    REFERENCES `mvc_intranet`.`Users` (`id_user`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Teachers_Specialities1`
     FOREIGN KEY (`id_speciality`)
-    REFERENCES `mydb`.`Specialities` (`id_speciality`)
+    REFERENCES `mvc_intranet`.`Specialities` (`id_speciality`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Plannings`
+-- Table `mvc_intranet`.`Plannings`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Plannings` (
+CREATE TABLE IF NOT EXISTS `mvc_intranet`.`Plannings` (
   `id_planning` INT NOT NULL AUTO_INCREMENT,
   `id_subject` INT NOT NULL,
   `id_teacher` INT NOT NULL,
@@ -174,26 +174,26 @@ CREATE TABLE IF NOT EXISTS `mydb`.`Plannings` (
   INDEX `fk_Plannings_Students1_idx` (`id_student` ASC),
   CONSTRAINT `fk_Plannings_Subjects1`
     FOREIGN KEY (`id_subject`)
-    REFERENCES `mydb`.`Subjects` (`id_subject`)
+    REFERENCES `mvc_intranet`.`Subjects` (`id_subject`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Plannings_Teachers1`
     FOREIGN KEY (`id_teacher`)
-    REFERENCES `mydb`.`Teachers` (`id_teacher`)
+    REFERENCES `mvc_intranet`.`Teachers` (`id_teacher`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Plannings_Students1`
     FOREIGN KEY (`id_student`)
-    REFERENCES `mydb`.`Students` (`id_student`)
+    REFERENCES `mvc_intranet`.`Students` (`id_student`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Classrooms`
+-- Table `mvc_intranet`.`Classrooms`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Classrooms` (
+CREATE TABLE IF NOT EXISTS `mvc_intranet`.`Classrooms` (
   `id_classroom` INT NOT NULL AUTO_INCREMENT,
   `classroom_name` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`id_classroom`))
@@ -201,9 +201,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Classrooms_to_Plannings`
+-- Table `mvc_intranet`.`Classrooms_to_Plannings`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Classrooms_to_Plannings` (
+CREATE TABLE IF NOT EXISTS `mvc_intranet`.`Classrooms_to_Plannings` (
   `id_classroom` INT NOT NULL,
   `id_planning` INT NOT NULL,
   PRIMARY KEY (`id_classroom`, `id_planning`),
@@ -211,21 +211,21 @@ CREATE TABLE IF NOT EXISTS `mydb`.`Classrooms_to_Plannings` (
   INDEX `fk_Classrooms_has_Plannings_Classrooms_idx` (`id_classroom` ASC),
   CONSTRAINT `fk_Classrooms_has_Plannings_Classrooms`
     FOREIGN KEY (`id_classroom`)
-    REFERENCES `mydb`.`Classrooms` (`id_classroom`)
+    REFERENCES `mvc_intranet`.`Classrooms` (`id_classroom`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Classrooms_has_Plannings_Plannings1`
     FOREIGN KEY (`id_planning`)
-    REFERENCES `mydb`.`Plannings` (`id_planning`)
+    REFERENCES `mvc_intranet`.`Plannings` (`id_planning`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Teachers_to_Promotions`
+-- Table `mvc_intranet`.`Teachers_to_Promotions`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Teachers_to_Promotions` (
+CREATE TABLE IF NOT EXISTS `mvc_intranet`.`Teachers_to_Promotions` (
   `id_teacher` INT NOT NULL,
   `id_promotion` INT NOT NULL,
   PRIMARY KEY (`id_teacher`, `id_promotion`),
@@ -233,21 +233,21 @@ CREATE TABLE IF NOT EXISTS `mydb`.`Teachers_to_Promotions` (
   INDEX `fk_Teachers_has_Promotions_Teachers1_idx` (`id_teacher` ASC),
   CONSTRAINT `fk_Teachers_has_Promotions_Teachers1`
     FOREIGN KEY (`id_teacher`)
-    REFERENCES `mydb`.`Teachers` (`id_teacher`)
+    REFERENCES `mvc_intranet`.`Teachers` (`id_teacher`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Teachers_has_Promotions_Promotions1`
     FOREIGN KEY (`id_promotion`)
-    REFERENCES `mydb`.`Promotions` (`id_promotion`)
+    REFERENCES `mvc_intranet`.`Promotions` (`id_promotion`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Spes`
+-- Table `mvc_intranet`.`Spes`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Spes` (
+CREATE TABLE IF NOT EXISTS `mvc_intranet`.`Spes` (
 )
 ENGINE = InnoDB;
 
