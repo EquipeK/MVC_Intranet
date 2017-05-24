7e4d50fa81efd23e2a90a7888c7e0a8a32f5c3c6
Christophe GRASSIOT <grassiot.christophe@gmail.com>
2017-05-24 14:34:16 +0200

    FIX Database

--- core/Database/MysqlDatabase.php
+++ core/Database/MysqlDatabase.php
@@ -11,16 +11,16 @@ class MysqlDatabase extends Database{
     private $db_host;
     private $pdo;
 
-    public function __construct($db_name, $db_user = 'root', $db_pass = '', $db_host = 'localhost'){
+    public function __construct($db_name, $db_user, $db_pass, $db_host){
         $this->db_name = $db_name;
-        $this->$db_user = $db_user;
-        $this->$db_pass = $db_pass;
-        $this->$db_host = $db_host;
+        $this->db_user = $db_user;
+        $this->db_pass = $db_pass;
+        $this->db_host = $db_host;
     }
 
     private function getPDO(){
         if($this->pdo === null){
-            $pdo = new PDO('mysql:dbname=bachelor;host=localhost', 'root', '');
+            $pdo = new PDO('mysql:dbname=' . $db_name . ';host=' . $db_host . '', $db_user, $db_pass);
             $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $this->pdo = $pdo;
         }
